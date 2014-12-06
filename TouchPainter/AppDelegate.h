9efc85b69b39be8d7a77a216522fdaecaaca1f97
周社军 <965678322@qq.com>
2014-12-06 19:59:16 +0800

    Initial Commit

---
+++ TouchPainter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchPainter
+//
+//  Created by zhoushejun on 14-12-6.
+//  Copyright (c) 2014年 shejun.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchPainter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchPainter
+//
+//  Created by zhoushejun on 14-12-6.
+//  Copyright (c) 2014年 shejun.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
